be22016f56ab627468baa60da8f2d3a75684a8ac
wmastersPlym <william.masters@students.plymouth.ac.uk>
2019-05-28 06:23:55 +0100

    Added basic server functionality with ability to connect to local mysql server and ejs page rendering

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql');
+
+
+const port = 80; // the port the server listens for
+app.listen(port);
+
+app.set("view engine", "ejs")
+
+const db = mysql.createConnection({ // Establishes connection with the database
+  host: 'localhost',
+  user: 'root',
+  password: "Sid_skater10",
+  database: "socialnetworkdb"
+});
+
+db.connect( function(err) { // Tests connection with the database
+   if(err) { // test for error
+      console.error("Error connectiong: " + err.stack);
+      return;
+   }
+   console.log("Connected to db");
+});
+
+
+
+
+app.get("/", function(req,res) { // Home page
+   res.render("home");
+});
+
+app.get("/user/:username", function(req,res) { // user page
+   // Querys database for the username in the url
+   db.query(`SELECT * FROM socialnetworkdb.user WHERE username = '${req.params.username}'`, function(error, results, fields) {
+      if(error)  { // checks for error with the query
+         console.error("db error");
+      }
+      if(results.length < 1) {
+         res.send("User doesn't exist");
+      } else {
+         res.send(results[0]);
+      }
+   });
+
+});
+
+/*
+
+INSERT INTO user (username, display_name, bio, dt_created, password_hash) values (
+	'admin',
+    'William Masters'
+    'The site creator',
+    dateTime.now(),
+    'abcdefg'
+);
+
+
+
+
+*/
